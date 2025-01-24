e92396605ee68890f9d8d4fb87f534a6c819a899
Ryuuujin <145089905+Ryuuujin@users.noreply.github.com>
2025-01-24 07:07:22 +0700

    Add files via upload

---
+++ p (1).sql
@@ -0,0 +1,214 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 24, 2025 at 01:01 AM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 5.6.32
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `p`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_absen`
+--
+
+CREATE TABLE `tb_absen` (
+  `id` int(11) NOT NULL,
+  `id_karyawan` varchar(255) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `waktu` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_absen`
+--
+
+INSERT INTO `tb_absen` (`id`, `id_karyawan`, `nama`, `waktu`) VALUES
+(16, '1920392912', 'Budi Sanjaya', 'Thursday, 10-09-2020 07:52:25 am'),
+(17, '192', 'Sarah Mutia', 'Thursday, 10-09-2020 07:54:45 am'),
+(18, '9', 'Abdul Muhlisin Sudirman', 'Sunday, 20-09-2020 01:31:05 pm'),
+(19, '1234214', 'dafa', 'Thursday, 23-01-2025 02:59:19 pm');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_daftar`
+--
+
+CREATE TABLE `tb_daftar` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_daftar`
+--
+
+INSERT INTO `tb_daftar` (`id`, `username`, `password`) VALUES
+(2, 'admin', 'admin'),
+(5, 'admin2', 'admin2');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_jabatan`
+--
+
+CREATE TABLE `tb_jabatan` (
+  `id` int(11) NOT NULL,
+  `jabatan` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_jabatan`
+--
+
+INSERT INTO `tb_jabatan` (`id`, `jabatan`) VALUES
+(3, 'CEO'),
+(4, 'CTO'),
+(5, 'CFO'),
+(6, 'WP Pemasaran'),
+(7, 'CMO'),
+(8, 'COO'),
+(9, 'Office Boy');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_karyawan`
+--
+
+CREATE TABLE `tb_karyawan` (
+  `id_karyawan` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `tmp_tgl_lahir` varchar(255) NOT NULL,
+  `jenkel` varchar(255) NOT NULL,
+  `agama` varchar(255) NOT NULL,
+  `alamat` text NOT NULL,
+  `no_tel` varchar(18) NOT NULL,
+  `jabatan` varchar(255) NOT NULL,
+  `foto` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_karyawan`
+--
+
+INSERT INTO `tb_karyawan` (`id_karyawan`, `username`, `password`, `nama`, `tmp_tgl_lahir`, `jenkel`, `agama`, `alamat`, `no_tel`, `jabatan`, `foto`) VALUES
+(0, 'j', '865c0c0b4ab0e063e5caa3387c1a8741', 'o', 'i', '--', '--', 'Sukaraja Bogor', '0854545345754', 'CEO', '23012025051418cinemacut_generated.png'),
+(9, 'Abdul', '827ccb0eea8a706c4c34a16891f84e7b', 'Abdul Muhlisin Sudirman', 'Klaten / 19-09-1994', 'Laki-laki', 'Islam', '', '0895635721923', 'CEO', '21092020072509employee1.png'),
+(123, 'jon', '202cb962ac59075b964b07152d234b70', 'jon', 'wdw', 'Laki-laki', 'Islam', 'wdw', 'wdw', 'CTO', '24012025005551flowchart_admin.png'),
+(192, 'sarah', '9e9d7a08e048e9d604b79460b54969c3', 'Sarah Mutia', 'Cianjur / 10-12-1992', 'Perempuan', 'Islam', '', '08128384848', 'CEO', '10092020025112employee3.png'),
+(1234214, 'dafa', '81dc9bdb52d04dc20036dbd8313ed055', 'dafa', 'bogor', 'Laki-laki', 'Islam', 'Sukaraja Bogor', '0854545345754', 'CEO', '23012025085702Tidak berjudul.png'),
+(999999999, 'bagas', 'ee776a18253721efe8a62e4abd29dc47', 'bagas a', 'Jakarta / 10-01-1990', 'Laki-laki', 'Islam', 'Jakarta', '0895628383333', 'Office Boy', '10092020024120employee3.png'),
+(1920392912, 'user', 'ee11cbb19052e40b07aac0ca060c23ee', 'Budi Sanjaya', 'Bekasi / 10-12-1980', 'Laki-laki', 'Kristen', '', '0895254859994', 'CEO', '10092020023942employee1.png');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_keterangan`
+--
+
+CREATE TABLE `tb_keterangan` (
+  `id` int(11) NOT NULL,
+  `id_karyawan` varchar(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `keterangan` varchar(255) NOT NULL,
+  `alasan` text NOT NULL,
+  `waktu` varchar(255) NOT NULL,
+  `bukti` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_keterangan`
+--
+
+INSERT INTO `tb_keterangan` (`id`, `id_karyawan`, `nama`, `keterangan`, `alasan`, `waktu`, `bukti`) VALUES
+(51, '9', 'Abdul Muhlisin', 'Sakit', 'Saya Sakit Pak', 'Thursday, 10-09-2020 07:53:23 am', '10092020025339suratket1.png'),
+(52, '999999999', 'bagas a', 'Izin', 'Mohon maaf bapak / ibu, untuk hari ini saya tidak bisa hadir, dikarenakan saya izin.', 'Thursday, 10-09-2020 07:55:20 am', '10092020025619suratket2.jpg');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_absen`
+--
+ALTER TABLE `tb_absen`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tb_daftar`
+--
+ALTER TABLE `tb_daftar`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tb_jabatan`
+--
+ALTER TABLE `tb_jabatan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tb_karyawan`
+--
+ALTER TABLE `tb_karyawan`
+  ADD PRIMARY KEY (`id_karyawan`);
+
+--
+-- Indexes for table `tb_keterangan`
+--
+ALTER TABLE `tb_keterangan`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tb_absen`
+--
+ALTER TABLE `tb_absen`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT for table `tb_daftar`
+--
+ALTER TABLE `tb_daftar`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `tb_jabatan`
+--
+ALTER TABLE `tb_jabatan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `tb_keterangan`
+--
+ALTER TABLE `tb_keterangan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
